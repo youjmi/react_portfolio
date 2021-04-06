dd98661c225e53d7eba7d9b1d8d3ac86af35dd9b
Young Ji Kim <youjmik@gmail.com>
2021-04-06 01:13:06 -0400

    centered social media links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,10 +5,12 @@ import {SocialIcon} from 'react-social-icons'
 function Footer() {
   return (
     <footer className="footer">
-      <span> &copy; Jeezy 2021</span>
-      <SocialIcon target= "_blank" url ="https://www.linkedin.com"/>
+          <SocialIcon target= "_blank" url ="https://www.linkedin.com"/>
       <SocialIcon target= "_blank" url ="https://www.github.com"/>
       <SocialIcon target= "_blank" url ="https://www.facebook.com"/>
+      <br></br>
+      <span> &copy; Jeezy 2021
+      </span>
     </footer>
   );
 }
