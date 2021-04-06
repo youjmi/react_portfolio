fb375559ae0553639962d16ad71722a946cab0db
Young Ji Kim <youjmik@gmail.com>
2021-04-06 17:54:58 -0400

    Worked out PDF bugs..

--- src/components/Slider/ResContent.js
+++ src/components/Slider/ResContent.js
@@ -1,5 +1,5 @@
-import Resume from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/YJK2021Resume.pdf"
-import CoverLetter from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/YJK2021CL.pdf"
+import Resume from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Young Ji Kim 2021 Resume.pdf"
+import CoverLetter from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Young Ji Kim 2021 CL.pdf"
 
 export const ResContent = [
   
