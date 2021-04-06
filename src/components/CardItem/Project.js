9c561eb92288969b8d01eb5e54f0dec77e91a962
Young Ji Kim <youjmik@gmail.com>
2021-04-06 18:28:11 -0400

    Modifying structure of Portfolios

--- src/components/CardItem/Project.js
+++ src/components/CardItem/Project.js
@@ -8,15 +8,18 @@ function Project(props) {
   return (
 
     <li className='cards__item holdbox'>
-      <div className='cards__item__link cardbox' to={props.path}>
+      <div className='cards__item__link cardbox'>
         <figure className='cards__item__pic-wrap'>
+         <a href ={props.deployed} target ="_blank">
           <img
             className='cards__item__img'
             alt='Assignment Image'
             src={props.src}
           />
+          </a>
         </figure>
         <div className='cards__item__info'>
+          <h3 className ="cards__title">{props.title}</h3>
           <h5 className='cards__item__text'>{props.text}</h5>
           <a href={props.github} target="_blank" 
             className="buttonCode buttonspace"> View Code </a>
--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -2,8 +2,8 @@
 import React from 'react';
 import "./Cards.css";
 import pic1 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Eddy.png"
-import pic2 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/employee-track.png"
-import pic3 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/employee-track.png"
+import pic2 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Budget.png"
+import pic3 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Workout.png"
 import pic4 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/employee-track.png"
 import pic5 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/employee-track.png"
 import pic6 from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/employee-track.png"
@@ -27,17 +27,17 @@ const Cards = () => {
             />
             <CardItem
               src={pic2}
-              title= ""
-              text='Employee Engine Generator'
-              github =""
-              deployed =""
+              title= "Offline and Online Budget Tracker"
+              text=''
+              github ="https://github.com/youjmi/Off-On_budget-Tracker"
+              deployed ="https://jeezy-budget-tracker.herokuapp.com/"
             />
             <CardItem
               src={pic3}
-              title= ""
-              text='Employee Engine Generator'
-              github =""
-              deployed =""
+              title= "Workout Tracker App'"
+              text=''
+              github ="https://github.com/youjmi/workout_Tracker-app"
+              deployed ="https://rocky-falls-71528.herokuapp.com/"
             />
           </ul>
           <ul className='cards__items'>
