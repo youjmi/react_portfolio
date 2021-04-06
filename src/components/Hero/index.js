3b1b3b44062454f2254988ae15b205f96b03c53f
Young Ji Kim <youjmik@gmail.com>
2021-04-06 00:40:13 -0400

    trying to push new video after clearing out histroy.. will probably have to redo alot of work

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import bgVid from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/video-1.mp4"
+import bgVid from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/video0-1.mp4"
 
 function Hero(props) {
   return (
