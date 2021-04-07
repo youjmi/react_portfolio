b1968506fe5367af2d69f1d0a7768f3c858cd2ab
Young Ji Kim <youjmik@gmail.com>
2021-04-06 22:15:12 -0400

    got buttons to be very well responsiveadd

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,9 +5,9 @@ import {SocialIcon} from 'react-social-icons'
 function Footer() {
   return (
     <footer className="footer">
-          <SocialIcon target= "_blank" className="iconSpace" url ="https://www.linkedin.com/in/young-ji-kim-a764b384/"/>
-      <SocialIcon className="gitHub" className="iconSpace" target= "_blank" bgColor="white" url ="https://github.com/youjmi"/>
-      <SocialIcon target= "_blank" url ="https://www.facebook.com"/>
+          <SocialIcon target= "_blank" className="iconSpace footicon" url ="https://www.linkedin.com/in/young-ji-kim-a764b384/"/>
+      <SocialIcon  className="iconSpace footicon1" target= "_blank" bgColor="white" url ="https://github.com/youjmi"/>
+      <SocialIcon className="footicon" target= "_blank" url ="https://www.facebook.com"/>
       <br></br>
       <span> &copy; Jeezy 2021
       </span>
