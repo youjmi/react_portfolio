077c730f5c6df1d594d72a12a36adb3d9b09a125
Young Ji Kim <youjmik@gmail.com>
2021-04-07 22:06:03 -0400

    looking good wiht Mobile Responsiveness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,14 +20,14 @@ function Home() {
           <Col className="colBorder">
               <img src={profilepic} alt="Jeezy" class="Jeezy"></img>
               </Col>
-              <Col className="colBorder">
-              <h1 className="introw">Hafa Adai and Hello!</h1>
-              <h1 className="introw">Thank you for visiting my website. </h1>
-              <h4 className="introw"> Welcome to my Portfolio. My name is Young Ji Kim and I am a Full Stack Developer currently
+              <Col className="colBordermain">
+              <h1 className="intro1">Hafa Adai and Hello!</h1>
+              <h1 className="intro1">Thank you for visiting my website. </h1>
+              <h4 className="intro2"> Welcome to my Portfolio. My name is Young Ji Kim and I am a Full Stack Developer currently
           attending GWU Bootcamp Coding Course, which will be completed at the end of April.</h4>
-              <h4 className="introw"> I was previously in the luxuy hotel management industry for about 8 years before the pandemic took
+              <h4 className="intro2"> I was previously in the luxuy hotel management industry for about 8 years before the pandemic took
           over. I am highly skilled in Customer Service, Sales, Operations and Project Management, Guest Relations and Employee Relations up to 74 Employees under a complex labor market.</h4>
-              <h4 className="introw"> I believe that, with my new Full Stack skills combined with my existing skills will create an immense wealth of diverse knowledge that I can provide for your company.  This webpage shows some of my works that I am most proud of!
+              <h4 className="intro2"> I believe that, with my new Full Stack skills combined with my existing skills will create an immense wealth of diverse knowledge that I can provide for your company.  This webpage shows some of my works that I am most proud of!
         </h4>
             
           </Col>
