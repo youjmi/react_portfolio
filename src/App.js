08d0725359e662cfdf6a61d2a6aec3660755b3f7
Young Ji Kim <youjmik@gmail.com>
2021-04-07 21:52:48 -0400

    Had to redo NavBar to make it mobile Responsive

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Portfolio from "./pages/Portfolio";
-import Resume from "./pages/Resume";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact"
 import Navbar from "./components/Navbar/index";
-import Footer from "./components/Footer/index";
 import Hero from "./components/Hero/index"
-import Wrapper from "./components/Wrapper/index";
+import Footer from "./components/Footer/index"
+
+
 
 function App() {
   return (
@@ -15,14 +12,8 @@ function App() {
     <Router basename={`${process.env.PUBLIC_URL}/`}>
       <div>
         <Navbar />
-        <Wrapper>
-          <Hero/>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/portfolio" component={Portfolio} />
-        <Route exact path="/resume" component={Resume} />
-        <Route exact path="/Contact" component={Contact} />
-        </Wrapper>
-        <Footer />
+        <Hero/>
+        <Footer/>
       </div>
     </Router>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,55 +1,67 @@
-import React, {useState} from "react";
-import { Link } from "react-router-dom";
+
 import "./style.css";
-import {Collapse, NavbarToggler} from "reactstrap"
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import Home from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/pages/Home";
+import Resume from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/pages/Resume";
+import Contact from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/pages/Contact";
+import Portfolio from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/pages/Portfolio";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
 
-  const [collapsed, setCollapsed] = useState(true);
 
-  const toggleNavBar = () => setCollapsed(!collapsed)
-
-  return (
-    <div >
-    <nav style ={{display:"flex" ,justifyContent:"center", alignItems:'center',height:"10vh"}}className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className=" nav-link nav-brand" to="/">
-        Jeezy
-      </Link>
-      {/* <NavbarToggler onClick={toggleNavBar}/>
-      <Collapse isOpen ={!collapsed}> */}
-        <ul className="navbar-nav">
-          <li className="nav-item">
-            <Link
-              to="/portfolio"
-              className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-            >
-              Portfolio
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/resume"
-              className={window.location.pathname === "/resume" ? "nav-link active" : "nav-link"}
-            >
-              Resume 
-            </Link>
-            
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/contact"
-              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-            >
-              Contact Me 
-            </Link>
-            
-          </li>
-        </ul>
-        {/* </Collapse> */}
-    </nav>
-    </div>
-  );
+class Navigation extends React.Component {
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<div className="col-md-12">
+						<Router>
+							<Navbar bg="light" variant="light" expand="lg" sticky="top">
+								<Navbar.Brand className ="nav-brand" href="/">Jeezy</Navbar.Brand>
+								<Navbar.Toggle aria-controls="basic-navbar-nav" />
+								<Navbar.Collapse id="basic-navbar-nav">
+									<Nav className="mr-auto main-nav ">
+										<Nav.Link className ="nav-link" href="/portfolio">Projects</Nav.Link>
+										<Nav.Link className ="nav-link" href="/resume">Resume</Nav.Link>
+										<Nav.Link className ="nav-link" href="/contact">Contact Me</Nav.Link>
+											
+									</Nav>
+									<Nav className="ml-auto">
+										<Nav.Link
+											target="_blank"
+											href="https://github.com/Alexfit4"
+										>
+											<i className="fab fa-github text-3xl "></i>
+										</Nav.Link>
+										<Nav.Link
+											target="_blank"
+											href="https://www.linkedin.com/in/amir-ashtiany-4a25421b9/"
+										>
+											<i className="fab fa-linkedin text-3xl "></i>
+										</Nav.Link>
+										<Nav.Link
+											target="_blank"
+											href="https://www.facebook.com/alex.ashtiany"
+										>
+											<i className="fab fa-facebook-square text-3xl "></i>
+										</Nav.Link>
+									</Nav>
+								</Navbar.Collapse>
+							</Navbar>
+							<Switch>
+								<Route component={Home} exact path="/" />
+								<Route component={Portfolio} path="/portfolio" />
+								<Route component={Resume} path="/resume" />
+								<Route component={Contact} path="/contact" />
+							</Switch>
+						</Router>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default Navbar;
+
+export default Navigation;
