8d7f2764b9c4762a2f482f8fc9a03bc5eb7aa206
Young Ji Kim <youjmik@gmail.com>
2021-04-08 17:26:36 -0400

    changed my resume and CL from PDF into google doc preview for faster loading

--- src/components/Slider/ResContent.js
+++ src/components/Slider/ResContent.js
@@ -1,25 +1,21 @@
-import Resume from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Young Ji Kim 2021 Resume.pdf"
-import CoverLetter from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Young Ji Kim 2021 CL.pdf"
+// import Resume from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Young Ji Kim 2021 Resume.pdf"
+// import CoverLetter from "/Users/youngjikim/Desktop/GitLab/gwu-arl-fsf-pt-10-2020-u-c/20-State/02-Homework/react_portfolio/src/images/Young Ji Kim 2021 CL.pdf"
 
 export const ResContent = [
 
-{
-  image: Resume
-},
-{
-  image: CoverLetter
-},
-
-
-
-
+// {
+//   image: Resume
+// },
+// {
+//   image: CoverLetter
+// },
   
-  //   {
-  //     image: "https://drive.google.com/file/d/1hROwmMqU2eWp6_jv8euiePRQJ8yN_-IX/preview?usp=drivesdk"
-  // },
-  // {
-  //     image: "https://drive.google.com/file/d/1end5RcB5rJ8zk06h0553ReWa_IGSs0oS/preview?usp=drivesdk"
-  // },
+    {
+      image: "https://drive.google.com/file/d/1hROwmMqU2eWp6_jv8euiePRQJ8yN_-IX/preview?usp=drivesdk"
+  },
+  {
+      image: "https://drive.google.com/file/d/1end5RcB5rJ8zk06h0553ReWa_IGSs0oS/preview?usp=drivesdk"
+  },
   
 
 ];
