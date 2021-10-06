a52d7be0b727d5ced1fae527a0d3b98da453156d
Young Ji Kim <youjmik@gmail.com>
2021-10-06 13:57:32 -0400

    Updating Bio

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -24,10 +24,10 @@ function Home() {
               <h1 className="intro1">Hafa Adai and Hello!</h1>
               <h1 className="intro1">Thank you for visiting my website. </h1>
               <br></br>
-              <h4 className="intro2"> Welcome to my Portfolio. My name is Young Ji Kim and I am a Full Stack Developer currently
-          attending GWU Bootcamp Coding Course, which will be completed at the end of April.</h4>
+              <h4 className="intro2"> Welcome to my Portfolio. My name is Young Ji Kim and I am a Full Stack Developer from completing the 
+           GWU Bootcamp Coding Course.</h4>
               <h4 className="intro2"> I was previously in the luxuy hotel management industry for about 8 years before the pandemic took
-          over. I am highly skilled in Customer Service, Sales, Operations and Project Management, Guest Relations and Employee Relations up to 74 Employees under a complex labor market.</h4>
+          over. I am highly skilled in Customer Service, Sales, Operations and Project Management, Guest Relations and Employee Relations under a complex labor market.</h4>
               <h4 className="intro2"> I believe that, with my new Full Stack skills combined with my existing skills will create an immense wealth of diverse knowledge that I can provide for your company.  This webpage shows some of my works that I am most proud of!
         </h4>    
           </Col>
